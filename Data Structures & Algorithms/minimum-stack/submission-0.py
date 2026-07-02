ccab1634df91cff8c20a7bc117597e68788c2e7c
Lincoln Maxwell <60242131+lllincoln@users.noreply.github.com>
2026-07-02 12:42:17 -0400

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,26 @@
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        
+        if len(self.min_stack) == 0:
+            self.min_stack.append(val)
+        elif self.min_stack[-1] >= val:
+            self.min_stack.append(val)
+        else:
+            pass
+
+    def pop(self) -> None:
+        val = self.stack.pop()
+
+        if self.min_stack[-1] == val:
+            self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
